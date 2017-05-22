47bebf92cc6736b08f1db33003b613dbaa23a338
Delveri Munang <chickdelveri@gmail.com>
2017-05-22 11:17:14 -0400

    Fixed Dashboard

--- routes/login.js
+++ routes/login.js
@@ -5,9 +5,14 @@ var mysql = require('mysql');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-res.render('login', { title: 'Wakku Login'});
+	res.render('login', { title: 'Wakku Login'});
 
   
+});
+
+router.post('/', function(req, res,next){
+		// res.render('home', {title : 'Welcome'});
+		res.redirect('/home');
 });
 
 module.exports = router;
