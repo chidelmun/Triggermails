86d6293cdf0a0a95f42c1c79ff32a8c6d6cbb537
Delveri Munang <chickdelveri@gmail.com>
2017-05-22 14:41:50 -0400

    Added connector to signup new user

--- routes/register.js
+++ routes/register.js
@@ -11,4 +11,29 @@ router.get('/', function(req, res, next) {
 	
 });
 
+router.post('/', function(req, res, next){
+	var connection = mysql.createConnection({
+		host : 'localhost',
+		user : 'root',
+		password : 'root',
+		database : 'mailapp'
+	});
+
+	connection.connect(function(err){
+		if (err) {
+			console.log("Error Connecting to Database");
+		}else{
+			console.log("Connected to Database");
+		}
+	});
+
+	connection.query("insert into users(email, display_name, password) values(?,?,?)",[req.body.user,req.body.fname + ' ' + req.body.lname, req.body.pass],function(err, results,fields){
+		if (err) {
+			console.log("Error Executing Query " + err);
+		}else{
+			res.send("Thank You");
+		}
+	});
+
+});
 module.exports = router;
