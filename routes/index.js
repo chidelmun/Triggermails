465c3797253f06443cd472d53f243aab9f5dd81d
Delveri Munang <chickdelveri@gmail.com>
2017-05-22 12:05:14 -0400

    Fixed Login with error reporting

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var session = require('express-session');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	if (!req.session.user) {
+		res.redirect('/login');
+	}
+	
+
+	req.session.user = {};
 	var connection = mysql.createConnection({
 		host : 'localhost',
 		user : 'root',
 		password : 'root',
-		database : 'shop'
+		database : 'mailapp'
 	});
 
 	connection.connect(function(err){
@@ -17,7 +24,7 @@ router.get('/', function(req, res, next) {
 		}
 	});
 
-	connection.query("SELECT * from jobs", function(err, results, fields){
+	connection.query("SELECT * from users", function(err, results, fields){
 		if (err) {
 			console.log("Error with Query " + err);
 		}else{
--- routes/login.js
+++ routes/login.js
@@ -5,14 +5,54 @@ var mysql = require('mysql');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-	res.render('login', { title: 'Wakku Login'});
+	res.render('login', { title: 'Wakku Login',  message : ''});
 
   
 });
 
 router.post('/', function(req, res,next){
-		// res.render('home', {title : 'Welcome'});
-		res.redirect('/home');
+
+
+		var username = req.body.user;
+		var password = req.body.pass;
+		console.log("Username : " + username + " Password : " + password);
+
+		var connection = mysql.createConnection({
+			host : "localhost",
+			user : "root",
+			password : "root",
+			database : "mailapp"
+		});
+
+		connection.connect(function(err){
+			if (err) {
+				console.log("Error Connecting to Database");
+				res.send("Error Connecting to Database");
+			}else{
+				console.log("Connected to Database ");
+			}
+		});
+
+		connection.query("select * from users where email = ?", [username], function(err, results,fields){
+			
+			console.log(results);
+			if (results.length > 0) {
+				if (results[0].email == username && results[0].password == password) {
+					console.log("User Found in Database");
+					res.render('home', {title : 'Trigger Mail | Dashboard'});
+				}else{
+
+					res.render('login', {title : 'Trigger Mail | Login', message : 'Enter Correct Password'})
+				}
+
+			}else{
+				console.log("Username not Found!");
+				res.send("No User Found");
+			}
+		});
+
+
+		
 });
 
 module.exports = router;
