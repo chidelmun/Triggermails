8df8ddc34129f34eda19b3b0746c0e00fda476c3
Delveri Munang <chickdelveri@gmail.com>
2017-06-20 14:49:20 -0400

    Added test to DR fuzz

--- selenium-test/server.js
+++ selenium-test/server.js
@@ -11,17 +11,18 @@ var driver = new webdriver.Builder()
     .withCapabilities(webdriver.Capabilities.chrome())
     .build();
 
-driver.get('http://localhost:3000/login');
+driver.get('http://digitalrenter.com/login');
 
  for (var i = 0; i < 20 ; i++) {
- 	driver.findElement(By.name('user')).sendKeys(i);
-	driver.findElement(By.name('pass')).sendKeys(i);
-	driver.findElement(By.name('login')).click();
+ 	driver.findElement(By.name('identity')).sendKeys(i);
+	driver.findElement(By.name('password')).sendKeys(i);
+	driver.findElement(By.className('btn-primary')).click();
 	if (i==18) {
 		driver.findElement(By.name('user')).sendKeys('admin@localhost.com');
 		driver.findElement(By.name('pass')).sendKeys('admin');
-		driver.findElement(By.name('login')).click();
+		driver.findElement(By.className('btn-primary')).click();
 	}
+	
  }
 
 driver.findElement(By.name('user')).sendKeys('root');
